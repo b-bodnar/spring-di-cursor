f5243e51d520d5f3b488d2fd73ee05e2ad960c0b
Bohdan Bodnar <iloveyoulviv@gmail.com>
2020-04-02 13:37:35 +0300

    Remove empty line

--- src/main/java/com/cursor/App.java
+++ src/main/java/com/cursor/App.java
@@ -1,11 +1,8 @@
 package com.cursor;
-
 import com.cursor.execute.Executor;
 
 public class App {
-
     public static void main(String[] args) {
-
         Executor.execute();
     }
 }
--- src/main/java/com/cursor/execute/Executor.java
+++ src/main/java/com/cursor/execute/Executor.java
@@ -6,9 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Executor {
-
     public static void execute() {
-
         ApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
         Scheduler scheduler = context.getBean(Scheduler.class);
         scheduler.getTime();
